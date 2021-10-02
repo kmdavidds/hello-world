52158548056d63952d1e64b727b2c769bf7ae25f
kmdavidds <91604889+kmdavidds@users.noreply.github.com>
2021-10-02 18:23:48 +0800

    Update FizzBuzz.py
    added tom scott

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,3 +1,4 @@
+# first try
 for i in range(1, 101):
     if i % 5 == 0 and i % 3 == 0:
         print("FizzBuzz")
@@ -7,3 +8,14 @@ for i in range(1, 101):
         print("Buzz")
     else:
         print(i)
+
+# tom scott
+for i in range(1, 101):
+    output = ""
+    if i % 3 == 0:
+        output += "Fizz"
+    if i % 5 == 0:
+        output += "Buzz"
+    if output == "":
+        output += str(i)
+    print(output)
